682bf468587de7f53cb9160bdc85716bd230b6e8
KarolTracz <106237947+KarolTracz@users.noreply.github.com>
2023-05-17 18:45:55 +0200

    Add files via upload
    Auto appending clickable product in list

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(executable_path="C:\\Users\\lolek\\chromedriver\\chromedriver.exe")
+driver = webdriver.Chrome()
 
 
 def start():
@@ -14,26 +14,18 @@ def start():
     lang = driver.find_element(By.ID, value="langSelect-EN")
     lang.click()
     driver.implicitly_wait(5)
+    BigCookie = driver.find_element(By.ID, value="bigCookie")
+    for _ in range(120):
+        BigCookie.click()
     return None
 
 
 start()
 time.sleep(1)
-BigCookie = driver.find_element(By.ID, value="bigCookie")
-
-
-def first_buy():
-    for _ in range(110):
-        BigCookie.click()
-    local_cursor = driver.find_element(By.ID, value="product0")
-    local_cursor.click()
-
-
-first_buy()
 
 
 class Product:
-    def __init__(self, name: str = 'NoName', amount: int = 0, price: int = 1, cps: float = 0, boost: float = 0, click=None):
+    def __init__(self, name: str = 'NoName', price: int = 1, amount: int = 0, cps: float = 0., boost: float = 0., click=None):
         """
         :param name: Product name as a string
         :param amount: How much product is owned
@@ -51,12 +43,12 @@ class Product:
 
     def __str__(self):
         return f"""
-name    {type(self.name)}{self.name}
-amount: {type(self.amount)}{self.amount}
-price:  {type(self.price)}{self.price}
-cps:    {type(self.cps)}{self.cps}
-boost:  {type(self.boost)}{self.boost}
-click_: {type(self.__click)}{self.__click}
+name    {self.name}
+price:  {self.price}
+amount: {self.amount}
+cps:    {self.cps}
+boost:  {self.boost}
+click_: {self.__click}
 """
 
     def buy(self):
@@ -70,33 +62,17 @@ click_: {type(self.__click)}{self.__click}
 
 products = driver.find_elements(By.CLASS_NAME, value="content")
 product_list = []
-product_dict = {}
-iteration = 0
+count = 0
 for product in products:
-    iteration += 1
+    count += 1
     list_ = product.text.split('\n')
     if list_[0] != '???' and list_[0] != '':
-        product_list.append(Product(list_[0]))
-    product_dict.update({f'product{iteration}': list_})
+        product_list.append(Product(list_[0], int(list_[1]), click=driver.find_element(By.ID, value=f"product{count}")))
 
-
-cursor = Product(
-    driver.find_element(By.ID, value="productName0").text,
-    int(driver.find_element(By.ID, value="productOwned0").text),
-    int(driver.find_element(By.ID, value="productPrice0").text),
-    click=driver.find_element(By.ID, value="product0"),
-    # driver.find_element(By.ID, value="productName0"),
-    # driver.find_element(By.ID, value="productName0"),
-)
-
-
-# def product_refresh():
-#     pass
+raw_cookies_source = driver.find_element(By.ID, value="cookies")
 
 
 def cookies_refresh():
-    raw_cookies_source = driver.find_element(By.ID, value="cookies")
-    driver.implicitly_wait(5)
     cookies_source = raw_cookies_source.text.replace(' cookies\nper second: ', ',').split(sep=',')
     cookies = int(cookies_source[0].replace(',', ''))
     cookies_per_second = float(cookies_source[1])
@@ -104,8 +80,11 @@ def cookies_refresh():
 
 
 for i in product_list:
-    print(i)
-print(product_dict)
+    i.buy()
+
+for _ in range(10):
+    time.sleep(10)
+    print(cookies_refresh())
 
 time.sleep(1)
 driver.close()
